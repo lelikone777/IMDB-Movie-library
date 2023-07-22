563fae6074019eb6a3e3b84c014d74e7cc53ba5a
lelikone777 <2257855@gmail.com>
2023-07-22 16:47:55 +0400

    svg

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Image from "next/image";
 
 const loading = () => {
     return (
         <div className='flex justify-center'>
-            <img style={{maxWidth: '40px'}} className='h-96' src="spinner.svg" alt="loafing..."/>
+            <Image width={100} height={100} src="spinner.svg" alt="loafing..."/>
         </div>
     );
 };
